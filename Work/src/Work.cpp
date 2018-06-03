8b4f10485b44bd3306dd15fdbfc1647ebf3701af
Heimanshu <hemnshhjoshi139.007@gmail.com>
2018-06-03 20:33:27 +0200

    this is new project

---
+++ Work/src/Work.cpp
@@ -0,0 +1,45 @@
+//============================================================================
+// Name        : Work.cpp
+// Author      : Heimanshu
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+class A
+{
+
+public:
+	void out()
+	{
+		cout<< "this is the hello world for Product owner----[Hello Product Owner]" <<endl;
+	}
+
+};
+
+class B
+{
+
+public:
+	void outpu()
+	{
+		cout<< "this is the hello world for Stakeholders ----[Hello StakeHolders]" <<endl;
+	}
+
+};
+
+int main()
+{
+
+    A a1;
+    B b1;
+    a1.out();
+    b1.outpu();
+    return 0;
+}
+
+
+
